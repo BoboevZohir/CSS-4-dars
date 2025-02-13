7137c5c3b62e7662480c56e84b6e60f0b95f98fc
Boboev Zohir <zohirbabaev151@gmail.com>
2025-02-13 09:37:36 +0500

    CSS 4-dars
    Zeromax It-center 40-guruh

---
+++ js/main2.js
@@ -0,0 +1,96 @@
+let box = document.getElementsByTagName("div").namedItem("box-1");
+let box1 = document.getElementsByTagName("div").namedItem("box-2");
+let a = document.getElementsByTagName("div").namedItem("a");
+let b = document.getElementsByTagName("div").namedItem("b");
+let c = document.getElementsByTagName("div").namedItem("c");
+let d = document.getElementsByTagName("div").namedItem("d");
+let e = document.getElementsByTagName("div").namedItem("e");
+let f = document.getElementsByTagName("div").namedItem("f");
+let g = document.getElementsByTagName("div").namedItem("g");
+let h = document.getElementsByTagName("div").namedItem("h");
+
+box.style.width = "1200px"
+box.style.margin = "20px auto"
+
+box1.style.width = "1200px"
+box1.style.margin = "20px auto"
+
+a.style.width = "200px"
+a.style.height = "200px"
+a.style.backgroundColor = "blue"
+a.style.color = "white"
+a.style.fontSize = "100px"
+a.style.textAlign = "center"
+a.style.lineHeight = "180px"
+a.style.margin = "0 10px"
+
+b.style.width = "200px"
+b.style.height = "250px"
+b.style.backgroundColor = "blue"
+b.style.color = "white"
+b.style.fontSize = "100px"
+b.style.textAlign = "center"
+b.style.lineHeight = "180px"
+b.style.margin = "0 220px"
+b.style.marginTop = "-200px"
+
+c.style.width = "200px"
+c.style.height = "350px"
+c.style.backgroundColor = "blue"
+c.style.color = "white"
+c.style.fontSize = "100px"
+c.style.textAlign = "center"
+c.style.lineHeight = "180px"
+c.style.margin = "0 430px"
+c.style.marginTop = "-250px"
+
+d.style.width = "200px"
+d.style.height = "300px"
+d.style.backgroundColor = "blue"
+d.style.color = "white"
+d.style.fontSize = "100px"
+d.style.textAlign = "center"
+d.style.lineHeight = "180px"
+d.style.margin = "0 640px"
+d.style.marginTop = "-350px"
+
+e.style.width = "200px"
+e.style.height = "250px"
+e.style.backgroundColor = "blue"
+e.style.color = "white"
+e.style.fontSize = "100px"
+e.style.textAlign = "center"
+e.style.lineHeight = "180px"
+e.style.margin = "0 10px"
+e.style.marginTop = "100px"
+
+f.style.width = "200px"
+f.style.height = "300px"
+f.style.backgroundColor = "blue"
+f.style.color = "white"
+f.style.fontSize = "100px"
+f.style.textAlign = "center"
+f.style.lineHeight = "180px"
+f.style.margin = "0 220px"
+f.style.marginTop = "-250px"
+
+g.style.width = "200px"
+g.style.height = "250px"
+g.style.backgroundColor = "blue"
+g.style.color = "white"
+g.style.fontSize = "100px"
+g.style.textAlign = "center"
+g.style.lineHeight = "180px"
+g.style.margin = "0 430px"
+g.style.marginTop = "-300px"
+
+h.style.width = "200px"
+h.style.height = "150"
+h.style.backgroundColor = "blue"
+h.style.color = "white"
+h.style.fontSize = "100px"
+h.style.textAlign = "center"
+h.style.lineHeight = "180px"
+h.style.margin = "600px"
+h.style.margin = "0 640px"
+h.style.marginTop = "-250px"
